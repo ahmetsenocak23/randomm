d29f373846a417a753dd7451f666f194c3625611
ahmetsenocak23 <167453206+ahmetsenocak23@users.noreply.github.com>
2025-06-12 16:22:23 +0300

    main.py

--- main.py
+++ main.py
@@ -18,15 +18,38 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 manager = DB_Manager(DATABASE)
 @bot.event
 async def on_ready():
-    print(f'Bot hazır! {bot.user} olarak giriş yapıldı.')
+    print(f'Bot hazır! {bot.user} olarak giriş yapıldı. ')
+
+@bot.event
+async def on_ready(): 
+    kanal_id = KANAL İD'NİZ BURAYA
+    kanal = bot.get_channel(kanal_id)
+    if kanal:
+        await kanal.send("Mehaba !helpme yazarak komutları öğrenebilirsin =)")
+    
 
 @bot.command(name='merhaba')
 async def start_command(ctx):
-    await ctx.send("Merhaba! ben film seçmenize yardımcı olabilecek bir botum seçin ve tadını çıkarın =) ")
+    await ctx.send("Merhaba! ben film seçmenize yardımcı olabilecek bir botum, seçin ve tadını çıkarın =) ")
+    
+@bot.command(name='helpme')
+async def start_command(ctx):
+    await ctx.send("Merhaba! **!movie** rastgele film seçmenizi , **!af2013** 2013 sonrası filmlerden rastgele seçmenizi \n **!be2013** se 2013 öncesi filmlerden rastgele seçmenize olanak tanır.")
+
 
 @bot.command(name='movie')
 async def movie_selector(ctx):
     await ctx.send(manager.random_movie()) 
+
+@bot.command(name="af2013")
+async def movie_selector_time(ctx):
+    await ctx.send(manager.random_movie_time())
+
+@bot.command(name="be2013")
+async def movie_selector_time2(ctx):
+    await ctx.send(manager.random_movie_time2())
+    
+
     
 
 bot.run(TOKEN)
